4fe7c36a5e4f0fd93701c3eccf204ef80493e848
RAHA Technologies <117338820+rahatechnologies@users.noreply.github.com>
2023-05-20 23:13:20 +0530

    Updated code for new project rock paper scissors

--- rockPaperScissorsGame.js
+++ rockPaperScissorsGame.js
@@ -5,13 +5,19 @@ const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
 
+const RESULT_DRAW = 'DRAW';
+const PLAYER_WINS = 'WINS';
+const PLAYER_LOST = 'LOST';
+
+let isGameRunning = false;
+// function expression
 const getPlayerChoice = function () {
   const selection = prompt(
     `${ROCK}, ${PAPER} or ${SCISSORS}?`,
     ''
   ).toUpperCase();
 
-  // validation
+  // User input validation
   if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
     // negative case
     alert(`Invalid Choice! We choose ${ROCK} for you!`);
@@ -20,10 +26,62 @@ const getPlayerChoice = function () {
   return selection;
 };
 
-// function getPlayerChoice() {}
+const getComputerChoice = function () {
+  const randomValue = Math.random();
+
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+// Function declaration
+// function getWinner (cChoice, pChoice) {
+// }
+
+// Function expression
+// const getWinner = function (cChoice, pChoice) {
+
+//  Arrow function
+
+const add = (a, b) => a + b;
+
+const getWinner = (cChoice, pChoice) =>
+  cChoice === pChoice
+    ? RESULT_DRAW
+    : (cChoice === ROCK && pChoice === PAPER) ||
+      (cChoice === PAPER && pChoice === SCISSORS) ||
+      (cChoice === SCISSORS && pChoice === ROCK)
+    ? PLAYER_WINS
+    : PLAYER_LOST;
+
+// if (cChoice === pChoice) {
+//   return RESULT_DRAW;
+// } else if (
+//   // || logical or
+//   // && logical and
+//   (cChoice === ROCK && pChoice === PAPER) ||
+//   (cChoice === PAPER && pChoice === SCISSORS) ||
+//   (cChoice === SCISSORS && pChoice === ROCK)
+// ) {
+//   return PLAYER_WINS;
+// } else {
+//   return PLAYER_LOST;
+// }
+// };
 
 startGameBtn.addEventListener('click', function () {
+  if (isGameRunning) {
+    return;
+  }
+  isGameRunning = true;
   console.log('Game is starting .....');
-  const playerSelection = getPlayerChoice();
-  console.log(playerSelection);
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+
+  const winner = getWinner(computerChoice, playerChoice);
+  console.log(winner);
 });
